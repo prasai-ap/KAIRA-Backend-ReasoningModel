ffde0892328e349813c281f0831086e6508503df
cod.r64 <prasaiaayus@gmail.com>
2026-04-17 22:04:13 +0545

    added google auth logic function

---
+++ app/services/google_auth_service.py
@@ -0,0 +1,16 @@
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
+from app.core.auth_config import GOOGLE_CLIENT_ID
+
+
+def verify_google_token(token: str):
+    info = id_token.verify_oauth2_token(token, requests.Request(), GOOGLE_CLIENT_ID)
+
+    return {
+        "email": info.get("email"),
+        "full_name": info.get("name"),
+        "google_sub": info.get("sub"),
+        "profile_image_url": info.get("picture"),
+        "email_verified": info.get("email_verified", False),
+    }
