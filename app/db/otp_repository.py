a90fd83caf2c5a6536880dd899d7cc876d3d9fa2
cod.r64 <prasaiaayus@gmail.com>
2026-04-17 19:00:27 +0545

    added queries function for otp

---
+++ app/db/otp_repository.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+
+from app.models.otp_models import OTPCode
+
+
+def create_otp_record(db: Session, **kwargs):
+    otp_record = OTPCode(**kwargs)
+    db.add(otp_record)
+    db.commit()
+    db.refresh(otp_record)
+    return otp_record
+
+
+def get_latest_otp(db: Session, email: str):
+    return (
+        db.query(OTPCode)
+        .filter(OTPCode.email == email)
+        .order_by(OTPCode.created_at.desc())
+        .first()
+    )
+
+
+def mark_otp_used(db: Session, otp_record: OTPCode):
+    otp_record.used = True
+    db.commit()
+    db.refresh(otp_record)
+    return otp_record
+
+
+def increment_attempt_count(db: Session, otp_record: OTPCode):
+    otp_record.attempt_count += 1
+    db.commit()
+    db.refresh(otp_record)
+    return otp_record
