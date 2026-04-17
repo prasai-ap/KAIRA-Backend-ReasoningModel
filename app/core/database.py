486352d291b49d2e583c1013db914be1b35cf7d2
cod.r64 <prasaiaayus@gmail.com>
2026-04-17 15:19:53 +0545

    added authentication configuration

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-DATABASE_URL = "postgresql+psycopg2://postgres:root@localhost:5433/kaira_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
