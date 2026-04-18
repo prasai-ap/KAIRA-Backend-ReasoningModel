0cadc41f7f8e6b5931bc27ed2054156d1a6be27e
cod.r64 <prasaiaayus@gmail.com>
2026-04-18 18:55:37 +0545

    updated route refernece to entry point

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI
 from app.api.astrology_routes import router as astrology_router
 from app.api.auth_routes import router as auth_router
+from app.models.user_astrology_models import UserAstrologyData
 
 app = FastAPI()
 
