e1c17db93bfcb231ff866a0f0135d96e104414eb
cod.r64 <prasaiaayus@gmail.com>
2026-06-18 19:46:38 +0545

    added rag configuration

---
+++ app/rag/config.py
@@ -0,0 +1,11 @@
+import os
+
+RAG_BOOK_PATH = os.getenv("RAG_BOOK_PATH")
+RAG_VECTOR_PATH = os.getenv("RAG_VECTOR_PATH")
+RAG_COLLECTION_NAME = os.getenv("RAG_COLLECTION_NAME")
+
+RAG_CHUNK_SIZE = int(os.getenv("RAG_CHUNK_SIZE"))
+RAG_CHUNK_OVERLAP = int(os.getenv("RAG_CHUNK_OVERLAP"))
+RAG_TOP_K = int(os.getenv("RAG_TOP_K"))
+
+RAG_EMBEDDING_MODEL = os.getenv("RAG_EMBEDDING_MODEL")
