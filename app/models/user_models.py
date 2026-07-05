be3b446784216083f2859294abbbabba103a4512
cod.r64 <prasaiaayus@gmail.com>
2026-07-05 17:41:04 +0545

    remigrated whole database for free use record

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import Column, String, Boolean, DateTime , Integer
+from sqlalchemy import Column, String, Boolean, DateTime, Integer
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
 
